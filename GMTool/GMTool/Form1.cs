dbe88d9034467e1ab7b93f5b7635888ecf6fcd7e
Valium <takeyourvalium@gmail.com>
2013-01-13 17:06:21 -0800

    You are a winnar.

--- GMTool/GMTool/Form1.cs
+++ GMTool/GMTool/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace GMTool
 {
@@ -21,8 +22,15 @@ namespace GMTool
         private void btnLogin_Click(object sender, EventArgs e)
         {
             string response = network.HttpPost("http://www.status3gaming.com/gmtool/", "user=" + txtUsername.Text + "&pass=" + txtPassword.Text);
-            MessageBox.Show(response, "Response", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
 
+            if(response == "0"){
+                MessageBox.Show("Failed Login", "Response", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+            else if (Regex.IsMatch(response, "[0-9A-Fa-f]{40}"))
+            {
+                //save the hash
+                MessageBox.Show("You are a winnar!", "Response", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
         }
     }
 }
